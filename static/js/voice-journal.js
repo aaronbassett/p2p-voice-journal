725d8d72577d0c2c1450d67461416aec6151ebd4
Aaron Bassett <me@aaronbassett.com>
2018-06-13 03:03:14 +0100

    Add ability to play back recordings

--- static/js/voice-journal.js
+++ static/js/voice-journal.js
@@ -1,8 +1,36 @@
+Array.prototype.random = function () {
+  return this[Math.floor(Math.random() * this.length)]
+}
 
 var vm = new Vue({
   el: '#voice-journal-app',
   data: {},
   asyncComputed: {
+    recordings: {
+      async get () {
+        const archive = new DatArchive(window.location)
+        let allRecordings = await archive.readdir('/recordings', {recursive: true, stat: true})
+        return allRecordings.filter(recording => recording.name.split(".")[1] == "mp3").map(recording => {
+          recording.stat['humanSize'] = filesize(recording.stat.size, {round: 2})
+          recording.stat['modifiedTime'] = moment(recording.stat.mtime).startOf().fromNow()
+          recording.stat['createdTime'] = moment(recording.stat.ctime).startOf().fromNow()
+          recording.stat['missingBlocks'] = (recording.stat.blocks - recording.stat.downloaded != 0)
+          recording.stat['color'] = [
+            'Vlt-card__image--indigo',
+            'Vlt-card__image--green',
+            'Vlt-card__image--teal',
+            'Vlt-card__image--purple',
+            'Vlt-card__image--orange',
+            'Vlt-card__image--blue',
+            'Vlt-card__image--blue-light',
+          ].random()
+          return {
+            name: recording.name,
+            stat: recording.stat
+          }
+        })
+      }
+    },
     archiveInfo: {
       async get () {
         const archive = new DatArchive(window.location)
@@ -10,7 +38,10 @@ var vm = new Vue({
         return archiveInfo
       },
       default: {
-        peers: 0
+        peers: 0,
+        version: 0,
+        mtime: 0,
+        size: 0
       }
     },
     async peers () {
@@ -21,11 +52,18 @@ var vm = new Vue({
     },
     async mtime () {
       let timestamp = await this.archiveInfo.mtime
-      return moment().startOf(timestamp).fromNow()
+      return moment(timestamp).startOf().fromNow()
     },
     async filesize () {
       let size = await this.archiveInfo.size
       return filesize(size, {round: 2})
     }
+  },
+  updated: function () {
+    Array.from(document.querySelectorAll('audio')).map(p => {
+      new Plyr(p, {
+        controls:['play', 'current-time', 'mute', 'volume', 'settings']
+      })
+    })
   }
 })
